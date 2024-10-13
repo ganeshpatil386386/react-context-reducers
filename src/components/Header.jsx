4e95b907815b6513db5a633d116f3f7c813b4df0
ganeshpatil386386 <ganeshpatil386386@gmail.com>
2024-10-13 16:25:03 +0530

    shopping cart context-reducers

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import {
+  Badge,
+  Container,
+  FormControl,
+  Navbar,
+  Nav,
+  Dropdown,
+} from "react-bootstrap";
+const Header = () => {
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand>
+          <a href=''>Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className='search'>
+          <FormControl
+            style={{ width: 500 }}
+            placeholder='serach products'
+            className='m-auto'
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown>
+            <Dropdown.Toggle>
+              {/* <FaShoppingCart color='white' fontSize='25px' /> */}
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is Empty!</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
